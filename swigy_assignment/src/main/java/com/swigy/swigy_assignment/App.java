26a5c97a5689ad7adf101847a65d869d9b64abf7
Rajneesh-9088 <rp8735685@gmail.com>
2024-07-09 02:12:55 +0530

    Project setup has been done

---
+++ swigy_assignment/src/main/java/com/swigy/swigy_assignment/App.java
@@ -0,0 +1,13 @@
+package com.swigy.swigy_assignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
